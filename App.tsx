b959f11b9f5ceb5ab260f2d97f436fc9ec96233a
Michael Wang <michaelwang11394@gmail.com>
2022-07-19 10:26:57 -0400

    Change active color

--- App.tsx
+++ App.tsx
@@ -26,7 +26,7 @@ export default function App() {
             // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
-          tabBarActiveTintColor: "tomato",
+          tabBarActiveTintColor: "#1A434D",
           tabBarInactiveTintColor: "gray",
           headerShown: false,
         })}
